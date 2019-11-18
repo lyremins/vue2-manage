a961e73c22e62ed2d04ca10d9c40e629a7edd003
hanzhichao <hanzhichao@kingsoft.com>
2019-11-18 16:00:01 +0800

    1.commit

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App'
 import router from './router'
 import store from './store/'
 import ElementUI from 'element-ui'
+import * as util from './util';
 
 import 'element-ui/lib/theme-chalk/index.css'
 import './style/css/font-awesome.min.css'
+// import './element-variables.scss'
 
 Vue.config.productionTip = false;
+Vue.prototype.$util = util;
 
 Vue.use(ElementUI);
 
